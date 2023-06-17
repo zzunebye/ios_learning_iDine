f2bf1ed39025a27f24792e4dd895979aaadec73f
Jun Young Bang <bhang96@gmail.com>
2023-06-17 15:06:33 +0800

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by June on 17/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
